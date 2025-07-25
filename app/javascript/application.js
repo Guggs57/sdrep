b13091d8712880498a31e3a3d36ce92843263133
Guggs57 <n_guggenbuhl@hotmail.com>
2025-07-25 20:16:12 +0200

    adding product + rails_rails admin

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
+import "rails_admin"
+import "@popperjs/core"
+import "bootstrap"
+import "@fortawesome/fontawesome-free"
+import "flatpickr"
+import "jquery"
+import "jquery-ui"
